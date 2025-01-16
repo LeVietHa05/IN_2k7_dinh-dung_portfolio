36dcb1d097dd06b9500d47dfd254ce783c78b6e8
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-01-16 16:11:08 +0700

    update gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -7,6 +7,11 @@ const images1 = [
     paintingResource: "Acrylics on canvas",
     imgWidth: 427,
     imgHeight: 320,
+    overoverlayText: [
+      "Through the symbolic variant, wild orchids under extreme, contrast painting style, “The Orchid” provokes an irritable sense of women under social norm, a fervent rising up of feminism yet the alluring and sexyness of curves.",
+      "During a visit to our city’s botanical garden, I stumbled upon a vibrant field of orchids, showcasing a variety of species—from moth and boat orchids to slipper orchids and even vanilla orchids. Each flower, with its distinctive form, exuded a unique beauty while collectively evoking a shared symbolism: femininity.",
+      "This imagery sparked a thought - how often do young artists, including myself, grapple with representing women and gender distinctions without resorting to over-sexualized depictions? Why not use the orchid as a metaphorical illustration of femininity, embodying its elegant curves and delicate beauty? And thus, the concept was born.",
+    ],
   },
   {
     imgSrc: "/gallery/6.png",
@@ -15,6 +20,10 @@ const images1 = [
     paintingResource: "Acrylic on canvas",
     imgWidth: 427,
     imgHeight: 644,
+    overoverlayText: [
+      "True to its name, only three shades - purple, blue, and green - were used to portray a simple vase of blooming flowers. As one of the early still-life paintings in my artistic journey, this piece offered me more than just the technique of painting a flower vase. It taught me how to balance thin and thick brushstrokes, blend colors seamlessly into the background, and create striking highlights and subtle drop shadows to enhance depth and dimension.",
+      "Observing the artpiece, I feel more related and reminisce with the hardship process and message behind the art rather than the final result itself.",
+    ],
   },
   {
     imgSrc: "/gallery/2.png",
@@ -23,6 +32,10 @@ const images1 = [
     paintingResource: "Acrylics on canvas",
     imgWidth: 427,
     imgHeight: 280,
+    overoverlayText: [
+      'Nature always conceals a quiet breath of beauty, and art serves as a powerful medium to capture, celebrate, and preserve these hidden treasures. "Blue Butterfly" is one such serendipitous moment where nature harmoniously intertwines with itself, creating a breathtaking tableau.',
+      'This piece represents an early exploration of painting real-life nature using vibrant acrylics. "Blue Butterfly" captures the dynamic energy of a blue butterfly delicately perched on radiant red peony flowers. The striking contrast between the bold red and vivid blue enhances the artwork’s visual allure, adding depth and vibrancy to the composition.',
+    ],
   },
   {
     imgSrc: "/gallery/9.png",
@@ -32,6 +45,11 @@ const images1 = [
     paintingResource: "Many materials combined: acrylic, glue and pearl mosaic",
     imgWidth: 427,
     imgHeight: 320,
+    overoverlayText: [
+      "One of the standout pieces in my recent collection, “Golden Jungles, Silver Seas,” marks my first personal mixed-media creation within the series.",
+      "The title is a direct translation of the well-known Vietnamese idiom “Rừng vàng, biển bạc”, where “golden jungles” symbolize the rich jungles teeming with valuable timber, medicinal plants, and diverse wildlife, while “silver seas” reflect the abundance of marine resources, including fish, seafood, and other treasures of the ocean.",
+      "Incorporating shimmering pearl mosaics, the artwork aims to celebrate the cultural identity of a Southeast Asian country while highlighting the resourceful beauty of its natural wildlife. Through this piece, I seek to present a more",
+    ],
   },
   {
     imgSrc: "/gallery/8.png",
@@ -40,6 +58,10 @@ const images1 = [
     paintingResource: "Acrylics on canvas",
     imgWidth: 427,
     imgHeight: 426,
+    overoverlayText: [
+      "The art piece captures a vivid desert sunset with cactus silhouettes against a backdrop of warm reds and cool blues, blending vibrant colors to evoke the tranquility and intensity of desert landscapes. The frayed traces of the paint add a raw, abstract and movemental effect on the art’s perception of different visitors.",
+      "The special aspects of the landscape (the frayed drips of paint) could parallelly be considered as an metaphorical way of displaying the invisible definition of passing time.",
+    ],
   },
 ];
 const images2 = [
@@ -50,6 +72,9 @@ const images2 = [
     paintingResource: "Oil paints on canvas",
     imgWidth: 464,
     imgHeight: 637,
+    overoverlayText: [
+      `“Tulip n Lemon” is one of my earlier attempts at illustrating special textures such as porcelain, fabric,... and the effective use of light in still life art pieces. The picture captures a striking arrangement of bright yellow tulips in a deep blue vase, accompanied by a single lemon resting on a draped cloth. The rich contrast between the warm yellow hues and cool blues draws the viewer's attention, evoking a sense of calm and balance. The composition highlights simplicity and elegance, emphasizing the natural beauty of everyday objects.`,
+    ],
   },
   {
     imgSrc: "/gallery/4.png",
@@ -58,6 +83,10 @@ const images2 = [
     paintingResource: "Oil paints on canvas",
     imgWidth: 464,
     imgHeight: 342,
+    overoverlayText: [
+      `"Rideau, Cruchon et Compotier" is a renowned painting by French Post-Impressionist artist Paul Cézanne. Created around 1893–1894, this work is considered a masterpiece of still-life painting and showcases Cézanne's innovative approach to composition, form, and color, which significantly influenced modern art.`,
+      `This masterpiece by Paul Cézanne has been a significant source of inspiration for me in mastering oil painting on canvas. In my view, "Rideau, Cruchon et Compotier" is far more than a depiction of an ordinary still-life table; it embodies the profound artistic vision of the artist. Cézanne's meticulous use of varied color tones to craft a dynamic background, his skillful rendering of the jars' luminous surfaces, and his nuanced portrayal of the irregular folds of the tablecloth all demonstrate his extraordinary attention to detail and innovative approach to composition.`,
+    ],
   },
   {
     imgSrc: "/gallery/7.png",
@@ -66,6 +95,10 @@ const images2 = [
     paintingResource: "Acrylics on canvas",
     imgWidth: 464,
     imgHeight: 733,
+    overoverlayText: [
+      `Taking inspiration from the harvesting season of oversea fruits’ farms on mass media, I aim to recreate a-pattern-life fabric on canvas with the main symbol of yellow lemon - an uncommon fruit in our Asian countries. In contrast to the bright yellow of the lemon, I decided to choose a plain yet high-colored blue for the background - raising the feeling of youth as well as assisting the idea of a pattern-like art piece. `,
+      `It might be like the sky, yet can also be the sea or even the plain colour of a huge fabric’s piece. Whatever it is, each viewer will hold a distinctive perspective on the lemon’s story, accreting the meaning for the picture.`,
+    ],
   },
   {
     imgSrc: "/gallery/5.png",
@@ -74,6 +107,10 @@ const images2 = [
     paintingResource: "Acrylic embossed on canvas",
     imgWidth: 464,
     imgHeight: 302,
+    overoverlayText: [
+      `Using the unique technique of embossing a thick acrylic layer on a blank canvas, “Koi Fish” is not only a creative exploration of texture and form but also a special artistic approach aimed at engaging a broader audience.`,
+      `The pop-up texture of every intricate pattern lends the piece a sense of boundless spontaneity while maintaining an alluring visual harmony. The depiction of the Koi fish pair symbolizes a profound sense of freedom within the expansive realm of nature, drawing viewers closer to its tranquil beauty and dynamic energy. This piece bridges the gap between my own artistic experimentation and the emotional resonance of natural imagery.`,
+    ],
   },
 ];
 
--- app/ui/overlayimage.tsx
+++ app/ui/overlayimage.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 interface OverlayimageProps {
   imgSrc: string;
@@ -7,6 +9,7 @@ interface OverlayimageProps {
   paintingResource: string;
   imgWidth: number;
   imgHeight: number;
+  overoverlayText: string[];
 }
 
 export default function Overlayimage({
@@ -16,9 +19,18 @@ export default function Overlayimage({
   paintingNameText,
   paintingResource,
   paintingSizeText,
+  overoverlayText,
 }: OverlayimageProps) {
+  const [isOverlayOverlayVisible, setOverlayOverlayVisible] = useState(false);
+
+  const handleImageClick = () => {
+    setOverlayOverlayVisible(true);
+  };
+
   return (
-    <div className={`group relative w-[${imgWidth}px] h-[${imgHeight}px]`}>
+    <div
+      className={`group relative w-[${imgWidth}px] h-[${imgHeight}px]`}
+      onClick={handleImageClick}>
       <Image
         src={imgSrc}
         height={imgHeight}
@@ -34,6 +46,53 @@ export default function Overlayimage({
           <div>{paintingResource}</div>
         </div>
       </div>
+      {isOverlayOverlayVisible && (
+        <div
+          className={`flex fixed inset-0 bg-black bg-opacity-75 items-center justify-center z-50`}>
+          <div className="relative bg-gray-800 p-4 gap-10 rounded-lg shadow-lg flex w-[1120px] p-6 pr-20">
+            <div
+              className="absolute top-2 right-2 text-gray-600 hover:cursor-pointer"
+              onClick={() => {
+                // setOverlayOverlayVisible(false);
+                setTimeout(() => {
+                  setOverlayOverlayVisible(false);
+                  console.log(isOverlayOverlayVisible);
+                }, 1);
+              }}>
+              <Image src={"/close.png"} width={44} height={44} alt=""></Image>
+            </div>
+            <div className="relative shrink-0 ">
+              <div className="shrink-0">
+                <Image
+                  src={imgSrc}
+                  height={imgHeight}
+                  width={imgWidth}
+                  alt={paintingNameText}
+                />
+              </div>
+              <div className=" opacity-100 absolute bottom-0 left-0 right-0 h-full  w-full p-5 bg-[#242F414D] flex flex-col justify-end">
+                <div className=" absolute inset-x-0 bottom-0 bg-[#1D1708CC] h-[180px] blur-[30px] rounded-tr-[200px]"></div>
+                <div className="absolute pb-5 bottom-0 text-amber-800 text-lg font-medium">
+                  <div className="text-amber-100 text-2xl font-semibold">
+                    {paintingNameText}
+                  </div>
+                  <div>{paintingSizeText}</div>
+                  <div>{paintingResource}</div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-4 text-gray-50 font-medium flex flex-col justify-center">
+              {overoverlayText.map((text, i) => {
+                return (
+                  <div key={i} className="pb-4">
+                    {text}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
